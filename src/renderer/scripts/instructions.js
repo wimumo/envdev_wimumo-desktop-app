eef10c51bf7c3ad58a3229d1a43db05f91684292
Rocío Madou <rocio.madou@ing.unlp.edu.ar>
2023-08-29 15:02:56 -0300

    Inicio de desarrollo de Calibración

--- src/renderer/scripts/instructions.js
+++ src/renderer/scripts/instructions.js
@@ -1,6 +1,6 @@
 window.api.send('get-iplocal');
 
 window.api.receive('iplocal', (data) => {
-    document.getElementById('instrucciones_iplocal').innerHTML =
+    document.getElementById('instructions_iplocal').innerHTML =
     "(si la autodetección funciona puede ser: <mark>" + data[0] + "</mark>)";
 });
