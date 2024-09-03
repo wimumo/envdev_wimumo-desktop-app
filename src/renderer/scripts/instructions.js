f55ed52ba8174a373511d6003438edd0f70d2ab7
Selén Oliva <solivamusci@gmail.com>
2024-09-03 16:30:06 -0300

    refactoring CSS base, dark y light.

--- src/renderer/scripts/instructions.js
+++ src/renderer/scripts/instructions.js
@@ -2,5 +2,6 @@ window.api.send('get-iplocal');
 
 window.api.receive('iplocal', (data) => {
     document.getElementById('instrucciones_iplocal').innerHTML =
-    "(si la autodetección funciona puede ser: <mark>" + data[0] + "</mark>)";
+    //"(si la autodetección funciona puede ser: <mark>" + data[0] + "</mark>)";
+    data[0];
 });
