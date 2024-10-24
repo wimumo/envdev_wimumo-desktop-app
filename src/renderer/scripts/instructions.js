44a538ac3a8d63c22f3fa3840b91753950df0cc3
Selén Oliva <solivamusci@gmail.com>
2024-10-24 04:57:44 -0300

    Refactoring en instrucciones

--- src/renderer/scripts/instructions.js
+++ src/renderer/scripts/instructions.js
@@ -7,17 +7,21 @@ window.api.receive('iplocal', (data) => {
         data[0];
 });
 
-// funciones de uso por el index
-const guiasMainDiv = document.getElementById('guiasMainDiv');
-const configGuideDiv = document.getElementById('configGuideDiv');
-const phoneConfigGuideDiv = document.getElementById('phoneConfigGuideDiv');
-const compConfigGuideDiv = document.getElementById('compConfigGuideDiv');
-const WIMUMOInfoDiv = document.getElementById('WIMUMOInfoDiv');
+/*
+*   Funciones para ver guias. Usadas por el indice
+*/
 
-const guideBackButtons = document.getElementById('guideBackButtons');
-const guideFooter = document.getElementById('guideFooter');
+const guiasMainDiv = document.getElementById('guiasMainDiv');               // Pagina principal de guias
+const configGuideDiv = document.getElementById('configGuideDiv');           // primera pantalla de congif wimumo
+const phoneConfigGuideDiv = document.getElementById('phoneConfigGuideDiv'); // guia con celular
+const compConfigGuideDiv = document.getElementById('compConfigGuideDiv');   // guia con misma maquina
+const WIMUMOInfoDiv = document.getElementById('WIMUMOInfoDiv');             // Informacion del wimumo
+
+const guideBackButtons = document.getElementById('guideBackButtons');       // Botones para volver atras de una guia
+const guideFooter = document.getElementById('guideFooter');                 // footer
 
 function hideAllGuides() {
+    /* Antes de mostrar una guia siempre se necesita esconder el resto */
     guiasMainDiv.setAttribute('hidden', '');
     configGuideDiv.setAttribute('hidden', '');
     phoneConfigGuideDiv.setAttribute('hidden', '');
@@ -27,50 +31,48 @@ function hideAllGuides() {
     guideFooter.setAttribute('hidden', '');
 }
 
-// Gias de configuracion
+function homeGuia() {
+    /* Muestra todas las guias */
+    hideAllGuides();
+
+    guiasMainDiv.removeAttribute('hidden');
+    guideFooter.removeAttribute('hidden');
+}
+
 function guiaConfig() {
+    /* Primera pagina de la guia de configuracion, delega a guiaConfigPhone o guiaConfigComp */
     hideAllGuides();
 
     configGuideDiv.removeAttribute('hidden');
 }
 function guiaConfigPhone() {
+    /* Guia de configuracion usando un telefono */
     hideAllGuides();
 
     phoneConfigGuideDiv.removeAttribute('hidden');
     guideBackButtons.removeAttribute('hidden');
 
-    // logica de los pasos
-    ///showStep(1);
-    setUpGuide(totalStepsPhone, stepIdsPhone, buttonIdsPhone, checkboxIdsPhone);
-    showStep(currentStep);
+    setUpGuide(totalStepsPhone, stepIdsPhone, buttonIdsPhone, checkboxIdsPhone);    // Preparar la guia dinamica
 }
 function guiaConfigComp() {
+    /* Guia de configuracion usando la misma maquina */
     hideAllGuides();
 
     compConfigGuideDiv.removeAttribute('hidden');
     guideBackButtons.removeAttribute('hidden');
 
-    // logica de los pasos
-    ///showStep(1);
-    setUpGuide(totalStepsComp, stepIdsComp, buttonIdsComp, checkboxIdsComp);
-    showStep(currentStep);
+    // Preparar la guia dinamica
+    setUpGuide(totalStepsComp, stepIdsComp, buttonIdsComp, checkboxIdsComp);        // Preparar la guia dinamica
 }
 
-// Que es wimumo
 function WIMUMOInfo() {
+    /* Informacion sobre el dispositivo */
     hideAllGuides();
 
     WIMUMOInfoDiv.removeAttribute('hidden');
     guideBackButtons.removeAttribute('hidden');
 }
 
-// Lista de guias
-function homeGuia() {
-    hideAllGuides();
-
-    guiasMainDiv.removeAttribute('hidden');
-    guideFooter.removeAttribute('hidden');
-}
 
 document.getElementById('WIMUMOPageButton').addEventListener('click', function () {
     window.open('https://gibic.ing.unlp.edu.ar/wimumo', '_blank');
@@ -78,42 +80,27 @@ document.getElementById('WIMUMOPageButton').addEventListener('click', function (
 
 
 /* 
-*   Logica guias de Configuracion de dispositivo 
+*   Logica para guias dinamicas 
 */
 
 // constantes de la guia con telefono
 const totalStepsPhone = 5;
 const stepIdsPhone = ['stepP1', 'stepP2', 'stepP3', 'stepP4', 'stepP5'];
 const buttonIdsPhone = ['nextButtonPh', 'prevButtonPh'];
-const checkboxIdsPhone = [
-    'confCheckPh1',
-    'confCheckPh2',
-    'confCheckPh3',
-    'confCheckPh4',
-    'confCheckPh5'
-];
-///const nextButtonPhone = document.getElementById('nextButtonPh');
-///const prevButtonPhone = document.getElementById('prevButtonPh');
+const checkboxIdsPhone = ['confCheckPh1', 'confCheckPh2', 'confCheckPh3', 'confCheckPh4', 'confCheckPh5'];
 
 // constantes de la guia con la maquina
 const totalStepsComp = 5;
 const stepIdsComp = ['stepC1', 'stepC2', 'stepC3', 'stepC4', 'stepC5'];
 const buttonIdsComp = ['nextButtonComp', 'prevButtonComp'];
-const checkboxIdsComp = [
-    'confCheckCo1',
-    'confCheckCo2',
-    'confCheckCo3',
-    'confCheckCo4',
-    'confCheckCo5'
-];
-///const nextButtonComp = document.getElementById('nextButtonComp');
-///const prevButtonComp = document.getElementById('prevButtonComp');
+const checkboxIdsComp = ['confCheckCo1', 'confCheckCo2', 'confCheckCo3', 'confCheckCo4', 'confCheckCo5'];
 
-// variables
+// variables dinamicas
 let currentStep = 1;
-let totalSteps; // se inicializa antes de usarse
+let totalSteps;
 let botonTerminarActivo = false;
 
+// referencias a la pagina actual
 const steps = [];
 let nextButton;
 let prevButton;
@@ -121,6 +108,9 @@ const checkboxes = [];
 
 
 function setUpGuide(totalStp, stpIds, btnIds, chkboxIds) {
+    /* Esta funcion existe para reiniciar e inicilizar todas las variables de una guia dinamica cuando se abre. */
+
+    // Pasos de la guia
     currentStep = 1;
     totalSteps = totalStp;
 
@@ -129,6 +119,7 @@ function setUpGuide(totalStp, stpIds, btnIds, chkboxIds) {
         steps.push(document.getElementById(id));
     });
 
+    // Referencias a botones de la guia
     nextButton = document.getElementById(btnIds[0]);
     prevButton = document.getElementById(btnIds[1]);
     toggleTerminarBtn(false);
@@ -139,14 +130,18 @@ function setUpGuide(totalStp, stpIds, btnIds, chkboxIds) {
         checkboxes.push(document.getElementById(id));
     });
 
+    // Agrega la funcionalidad a cada checkbox
     checkboxes.forEach((checkbox, index) => {
-        // Agrega la funcionalidad a cada checkbox
         checkbox.addEventListener('change', function () {
-            onChange(this.checked, index + 1);  // Pass checked state and index (1-based)
+            onChange(this.checked, index + 1);
         });
     });
+
+    // Inicializa la guia en el primer paso
+    showStep(currentStep);
 }
 
+// Funciones utiles
 function toggleTerminarBtn(bool) {
     if (bool) {
         botonTerminarActivo = true;
@@ -157,7 +152,7 @@ function toggleTerminarBtn(bool) {
     }
 }
 
-function toggleVisibilityPreviousBtn(bool) {
+function togglePreviousBtn(bool) {
     if (bool) {
         prevButton.setAttribute('hidden', '');
     } else {
@@ -171,76 +166,69 @@ function uncheckAllCheckboxes() {
     });
 }
 
-
+// Funcion para mostrar pasos
 function showStep(step) {
     /* Se encarga de mostrar el paso activo en un guia, toma como parametro un String que determina la guia*/
-    // Esconder todos los pasos
-    document.querySelectorAll('.step').forEach(step => step.classList.remove('active'));
 
-    // Show the current step
-    //const currentStepElement = document.getElementById(`step${step}`);
+    document.querySelectorAll('.step').forEach(step => step.classList.remove('active'));    // Esconder todos los pasos
+
     const currentStepElement = steps[step - 1];
-    currentStepElement.classList.add('active');
+    currentStepElement.classList.add('active');             // Muestra el paso actual
 
-    // Hide the "previous" button if it is the first step
-    toggleVisibilityPreviousBtn(step === 1);
+    togglePreviousBtn(step === 1);                          // Esconde el boton "previo" en el primer paso
+
+    checkboxes.forEach((checkbox, index) => {
+        if ((index + 1) < step) checkbox.checked = true;    // Checkea las checkboxes previas por si hubo un salto
+    })
 
     currentStep = step;
 }
 
-function nextButtonOnClick() {
-    if (botonTerminarActivo) { homeGuia() };
-    if (currentStep <= totalSteps) {
-        checkboxes[(currentStep - 1)].checked = true;
-        checkCheckbox(currentStep);
-    }
-}
+// Checkbox handlers
 
-function prevButtonOnClick() {
-    if (currentStep > 1) { currentStep-- }
-    checkboxes[currentStep - 1].checked = false;
-    uncheckCheckbox(currentStep);
-}
-
-
-// Checkbox handling
-
-function checkCheckbox(step) {
+function onCheckCheckbox(step) {
     if ((step + 1) <= totalSteps) {
         showStep(step + 1);
-    } else {
-        // Actualizar en el ultimo paso
-        if (step === totalSteps) {
-            showStep(totalSteps);
-            toggleTerminarBtn(true);
-        }
+    } else if (step === totalSteps) {
+        showStep(totalSteps);
+        toggleTerminarBtn(true);                            // Muestra el boton Terminar en el ultimo paso
     }
-
-    // Permite checkear varias checkboxes al mismo tiempo o ir varios pasos para atras
-    checkboxes.forEach((checkbox, index) => {
-        if ((index + 1) < step) checkbox.checked = true;
-    })
 }
 
-function uncheckCheckbox(step) {
+function onUncheckCheckbox(step) {
     showStep(step);
 
-    // Permite ir varios pasos para atras
     checkboxes.forEach((checkbox, index) => {
-        if ((index + 1) > step) checkbox.checked = false;
+        if ((index + 1) > step) checkbox.checked = false;   // Permite ir varios pasos para atras si hubo un salto
     })
 
-    // Volver del ultimo paso
-    if (botonTerminarActivo) {
-        toggleTerminarBtn(false)
+    if (botonTerminarActivo) {                              
+        toggleTerminarBtn(false);                            // Permite volver desde el ultimo paso
     }
 }
 
 function onChange(checked, checkboxId) {
     if (checked) {
-        checkCheckbox(checkboxId);  // Call the function for checked state
+        onCheckCheckbox(checkboxId);
     } else {
-        uncheckCheckbox(checkboxId);  // Call the function for unchecked state
+        onUncheckCheckbox(checkboxId);
     }
 }
 
+// Button handlers
+
+function nextButtonOnClick() {
+    if (botonTerminarActivo) { homeGuia() };                // Si es el ultimo paso entonces vuelve al inicio
+    if (currentStep <= totalSteps) {
+        checkboxes[(currentStep - 1)].checked = true;
+        onCheckCheckbox(currentStep);
+    }
+}
+
+function prevButtonOnClick() {
+    if (currentStep > 1) {
+        currentStep--;
+        checkboxes[currentStep - 1].checked = false;
+        onUncheckCheckbox(currentStep);
+    }
+}
